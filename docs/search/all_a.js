c74de0c012200b912a7f8717d11c41b5addb14b0
Dan Podeanu <pdan@esync.org>
2024-09-23 00:20:42 +0800

    update docs

--- docs/search/all_a.js
+++ docs/search/all_a.js
@@ -1,16 +1,17 @@
 var searchData=
 [
   ['saddr_0',['saddr',['../structsettings.html#ab0ae900aaaa17fc9a66df172ee8ae102',1,'settings']]],
-  ['security_1',['Security',['../md__r_e_a_d_m_e.html#autotoc_md10',1,'Listener security'],['../md__r_e_a_d_m_e.html#autotoc_md1',1,'Security']]],
-  ['sender_2',['Sender',['../md__r_e_a_d_m_e.html#autotoc_md9',1,'']]],
-  ['settings_3',['settings',['../structsettings.html',1,'']]],
-  ['settings_5finitialize_4',['settings_initialize',['../udp-redirect_8c.html#a7a34b83efc5dbf0c7a938d9f07fdbccb',1,'udp-redirect.c']]],
-  ['sif_5',['sif',['../structsettings.html#aca05989209d3cb086986065afacf88b6',1,'settings']]],
-  ['socket_5fsetup_6',['socket_setup',['../udp-redirect_8c.html#aca406131bb636c31134d300e0caae9b6',1,'udp-redirect.c']]],
-  ['sport_7',['sport',['../structsettings.html#a6144aeafdfb423cabed5f69eb96eb300',1,'settings']]],
-  ['statistics_8',['statistics',['../structstatistics.html',1,'']]],
-  ['statistics_5fdelay_5fseconds_9',['STATISTICS_DELAY_SECONDS',['../udp-redirect_8c.html#aa1a29c97612ba647f485fae49439e74f',1,'udp-redirect.c']]],
-  ['statistics_5fdisplay_10',['statistics_display',['../udp-redirect_8c.html#af333d2c7ec6c46fdf5ba1ede197c9fc4',1,'udp-redirect.c']]],
-  ['statistics_5finitialize_11',['statistics_initialize',['../udp-redirect_8c.html#af6d101333c4654c64f400cedab535cbe',1,'udp-redirect.c']]],
-  ['stats_12',['stats',['../structsettings.html#a75f657baf41d582a2481708f1e89e135',1,'settings']]]
+  ['security_1',['Security',['../md__r_e_a_d_m_e.html#autotoc_md1',1,'']]],
+  ['security_2',['Listener security',['../md__r_e_a_d_m_e.html#autotoc_md10',1,'']]],
+  ['sender_3',['Sender',['../md__r_e_a_d_m_e.html#autotoc_md9',1,'']]],
+  ['settings_4',['settings',['../structsettings.html',1,'']]],
+  ['settings_5finitialize_5',['settings_initialize',['../udp-redirect_8c.html#a7a34b83efc5dbf0c7a938d9f07fdbccb',1,'udp-redirect.c']]],
+  ['sif_6',['sif',['../structsettings.html#aca05989209d3cb086986065afacf88b6',1,'settings']]],
+  ['socket_5fsetup_7',['socket_setup',['../udp-redirect_8c.html#aca406131bb636c31134d300e0caae9b6',1,'udp-redirect.c']]],
+  ['sport_8',['sport',['../structsettings.html#a6144aeafdfb423cabed5f69eb96eb300',1,'settings']]],
+  ['statistics_9',['statistics',['../structstatistics.html',1,'']]],
+  ['statistics_5fdelay_5fseconds_10',['STATISTICS_DELAY_SECONDS',['../udp-redirect_8c.html#aa1a29c97612ba647f485fae49439e74f',1,'udp-redirect.c']]],
+  ['statistics_5fdisplay_11',['statistics_display',['../udp-redirect_8c.html#af333d2c7ec6c46fdf5ba1ede197c9fc4',1,'udp-redirect.c']]],
+  ['statistics_5finitialize_12',['statistics_initialize',['../udp-redirect_8c.html#af6d101333c4654c64f400cedab535cbe',1,'udp-redirect.c']]],
+  ['stats_13',['stats',['../structsettings.html#a75f657baf41d582a2481708f1e89e135',1,'settings']]]
 ];
